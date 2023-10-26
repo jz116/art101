9757d7ba1d1897d9cc3efaf82f49cf1ef065d951
jz116 <jiechengzhu2002@gmail.com>
2023-10-26 12:19:43 -0700

    Lab 5 Submit
    Upload all lab5 files at once.

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,22 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/*
+ * index.js - Lab 5: Data Types and Variables
+ * Author: Jiecheng Zhu
+ * Date: Oct. 25, 2023
+ * License: Public Domain
+ */
 
-// Constants
+// Define Variables
+make = "Ford";
+model = "Taurus";
+color = "Rusty";
+year = 1995;
 
-// Functions
+// calculate
+age = 2023 - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "years<br>");
