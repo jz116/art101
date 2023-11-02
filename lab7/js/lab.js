80359b99979ebd30d936be5fd1eb49b73a27f55d
jz116 <jiechengzhu2002@gmail.com>
2023-11-01 22:42:52 -0700

    Lab 7 Submit
    Upload all lab7 files at once.

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -1,21 +1,27 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
-
-// Constants
+/*
+ * index.js - Lab 7 - Functions
+ * Author: Jiecheng Zhu
+ * Date: Nov. 1, 2023
+ * License: Public Domain
+ */
 
 // Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+// sortUserName - a function that takes user input and sorts the letters of their name
+function sortUserName() {
+  var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
+  console.log ("userName =", userName);
+  // split string to array
+  var nameArray = userName.split('');
+  console.log ("nameArray =", nameArray);
+  // sort the array
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort =", nameArraySort);
+  document.writeln("Here is your sorted name: ", nameArraySort, "</br>");
+  // join array back to a string
+  var nameSorted = nameArraySort.join('');
+  console.log ("nameSorted =", nameSorted);
+  // return result
+  return nameSorted;
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+document.writeln("Oh hey, I've fixed your name: ", sortUserName(), "</br>");
