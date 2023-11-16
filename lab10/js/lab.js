09dc8e50537d9b7e06b9aa004cf44af1a862b66e
jz116 <jiechengzhu2002@gmail.com>
2023-11-16 13:13:52 -0800

    Add lab10 files
    Add lab10 files

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -1,21 +1,26 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
-
-// Constants
+/*
+ * index.js - Lab 10 - JavaScript for the Web
+ * Author: Jiecheng Zhu
+ * Date: Nov. 17, 2023
+ * License: Public Domain
+ */
 
 // Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+function generateRandomText() {
+  const text = "The customer is very important, the customer will be followed by the customer. But at the same time they happened with great labor and pain. For to come to the smallest detail, no one should practice any kind of work unless he derives some benefit from it.";
+  const min = 3;
+  const max = 100;
+  const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+  // Get a random starting index to slice the Lorem Ipsum text
+  const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+  // Generate the random Lorem Ipsum-like text
+  return text.slice(randStart, randStart + randLen);
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// click listener for button
+$("#make-convo").click(function(){
+  // get new fake dialogue
+  const newText = generateRandomText();
+  // append a new div to our output div
+  $("#output").append('<div class="text"><p>' + newText + '</p></div>');
+});
