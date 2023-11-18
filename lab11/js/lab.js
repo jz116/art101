e1d246aaa5d043ec5d21e3e64484c87b99d5265e
jz116 <jiechengzhu2002@gmail.com>
2023-11-18 11:43:14 -0800

    Add lab11 files
    Add lab11 files

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,21 +1,23 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
-
-// Constants
+/*
+ * index.js - Lab 11 - JavaScript Events and Forms
+ * Author: Jiecheng Zhu
+ * Date: Nov. 18, 2023
+ * License: Public Domain
+ */
 
 // Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+// Sorts the characters of a string in alphabetical order.
+function sortString(inputString) {
+  // We have to convert our string to an array and back again to sort it
+  return inputString.split('').sort().join('');
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// click listener for button
+$("#submit").click(function(){
+  // get value of input field
+  const userName = $("#user-name").val();
+  // now let's sort it
+  userNameSorted = sortString(userName);
+  // append a new div to our output div
+  $("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
+});
