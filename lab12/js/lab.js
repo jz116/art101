af936e3c489cdb996dd760c146e618ba565edebc
jz116 <jiechengzhu2002@gmail.com>
2023-11-22 09:11:46 -0800

    Add Lab 12 Files
    Add Lab 12 Files

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -1,21 +1,32 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
-
-// Constants
+/*
+ * index.js - Lab 12 - Conditionals
+ * Author: Jiecheng Zhu
+ * Date: Nov. 21, 2023
+ * License: Public Domain
+ */
 
 // Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+function sortingHat(str) {
+  len = str.length;
+  mod = len % 4;
+  if (mod == 0) {
+    return "Gryffindor"
+  }
+  else if (mod == 1) {
+    return "Ravenclaw"
+  }
+  else if (mod == 2) {
+    return "Slytherin"
+  }
+  else if (mod == 3) {
+    return "Hufflepuff"
+  }
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+var myButton = document.getElementById ("button") ;
+myButton.addEventListener ("click", function() {
+  var name = document.getElementById("input").value;
+  var house = sortingHat (name);
+  newText = "<p>The Sorting Hat has sorted you into " + house + "</p>";
+  document.getElementById ("output"). innerHTML = newText;
+})
