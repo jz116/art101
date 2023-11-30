118ddbd7af60cea58584cc1071e0af007a33a21d
jz116 <jiechengzhu2002@gmail.com>
2023-11-29 22:41:19 -0800

    Add Lab 14 Files
    Add Lab 14 Files

--- lab14/js/lab.js
+++ lab14/js/lab.js
@@ -1,21 +1,24 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
-
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+/*
+ * index.js - Lab 14 - Debugging Tools & Strategies (Lab 7 Debugging)
+ * Author: Jiecheng Zhu
+ * Date: Nov. 28, 2023
+ * License: Public Domain
+ */
+// sortUserName - a function that takes user input and sorts the letters of their name
+function sortUserName() {
+  var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
+  console.log ("userName =", userName);
+  // split string to array
+  var nameArray = userName.split('');
+  console.log ("nameArray =", nameArray);
+  // sort the array
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort =", nameArraySort);
+  document.writeln("Here is your sorted name: ", nameArraySort, "</br>");
+  // join array back to a string
+  var nameSorted = nameArraySort.join('');
+  console.log ("nameSorted =", nameSorted);
+  // return result
+  return nameSorted;
 }
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+document.writeln("Oh hey, I've fixed your name: ", sortUserName(), "</br>");
