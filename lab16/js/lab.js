4a604335b38be4763f5299c47cf558ce0829992d
jz116 <jiechengzhu2002@gmail.com>
2023-12-07 12:54:47 -0800

    Add Lab 16 Files
    Add Lab 16 Files

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -1,21 +1,41 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/*
+ * index.js - Lab 16 - JSON and APIs (XKCD comic!)
+ * Author: Jiecheng Zhu
+ * Date: Dec. 6, 2023
+ * License: Public Domain
+ */
 
-// Constants
+const endPoint = "https://api.nasa.gov/planetary/apod";
+const apiKey = "BRj9Y1GnsfawWt7uej3MJ0HbY5XNsG0JAFkilD0g";
 
-// Functions
+// add button event listener
+$(document).ready(function(){
+  $("#generate").click(function(){
+    console.log("Click");
+    // construct ajax object
+    const ajaxParams = {
+      url: endPoint,
+      data: {api_key: apiKey},
+      type: "GET",
+      dataType: "json",
+      success: ajaxSuccess,
+      error: ajaxError
+    }
+    $.ajax(ajaxParams);
+  });
+});
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+// functions
+function ajaxSuccess(comicObj){
+  console.log("Here's what I got:", comicObj);
+  const title = comicObj.title;
+  const desc = comicObj.explanation;
+  const imageURL = comicObj.url;
+  $("#output").append("<h3>" + title + "</h3>");
+  $("#output").append("<img src='" + imageURL + "' />");
+  $("#output").append("<p>" + desc + "</p>");
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
+function ajaxError(request, error){
+  console.log("Oops:", request, error);
 }
-
-// let's get this party started
-main();
